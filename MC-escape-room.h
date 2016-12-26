54a12eaf90e2a8ba3c973039257da9868acaba00
Justin Ong <justin.ong.97@gmail.com>
2016-12-26 19:09:45 +0800

    built basic functionality with code and rfid card pattern

--- MC-escape-room.h
+++ MC-escape-room.h
@@ -33,9 +33,37 @@ char keys[KEYPAD_ROWS][KEYPAD_COLS] = {
  * === Config for MFRC522 ===
  */
 #define UID_LENGTH 4
-#define MFRC522_NUM 4
+#define MFRC522_NUM 3
 
-byte mfrc522_ss_pins[] = {4, 5, 6, 7};
+byte mfrc522_ss_pins[] = {4, 6, 7};
+
+// For memory structure of the mifare classic 1kb cards,
+// http://www.nxp.com/documents/application_note/AN1304.pdf
+/*
+ * Mifare classic 1kb has 16 sectors, each sector has 4 blocks, each block storing 16 bytes
+ */
+// sector to store our data in
+#define DATA_SECTOR 1
+
+// trailer block of the above sector that will be authenticated against
+// indexed from start of card
+#define DATA_TRAILER_BLOCK 7
+
+// block to store our data in
+// indexed from start of card
+#define DATA_BLOCK 4
+
+// signature byte used to indicate that this system has stored data
+#define DATA_SIGNATURE 0xA5
+
+// byte index to store DATA_SIGNATURE in
+#define DATA_SIGNATURE_INDEX 1
+
+// byte index to store our data in
+#define DATA_INDEX 2
+
+// how long before a "correct reading" expires
+#define READER_TIMEOUT 500
 
 /*
  * === Config for state 0: waiting for code ===
@@ -44,3 +72,10 @@ byte mfrc522_ss_pins[] = {4, 5, 6, 7};
 #define MAX_CODE_LENGTH 4
 #define CORRECT_CODE_LENGTH 3
 char correct_code[CORRECT_CODE_LENGTH] = {'1', '2', '3'};
+
+/*
+ * === Config for state 100: debug mode ===
+ */
+
+#define DEBUG_SEQUENCE_LENGTH 8
+char debug_sequence[DEBUG_SEQUENCE_LENGTH] = {'2', '2', '8', '8', '4', '6', '4', '6'};
--- MC-escape-room.ino
+++ MC-escape-room.ino
@@ -1,6 +1,6 @@
 #include "MC-escape-room.h"
 
-MFRC522 *mfrc522[MFRC522_NUM];
+MFRC522 mfrc522[MFRC522_NUM];
 Keypad keypad = Keypad( makeKeymap(keys), keypad_row_pins, keypad_col_pins, KEYPAD_ROWS, KEYPAD_COLS );
 
 LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
@@ -15,23 +15,108 @@ void print_uid(byte *uid, int column, int row) {
 }
 
 void reset_ss_pins(void) {
-  // http://forum.arduino.cc/index.php?topic=27986.msg207074#msg207074
-  // 10 needs to be HIGH when SPI is reading or it'll hang for some odd reason
   pinMode(10, OUTPUT);
   digitalWrite(10, HIGH);
+  pinMode(MOSI, OUTPUT);
+  pinMode(SCK, OUTPUT);
   for (byte i = 0; i < MFRC522_NUM; i++) {
     digitalWrite(mfrc522_ss_pins[i], HIGH);
   }
 }
 
+byte read_rfid_reader(byte i, byte * data) {
+  /* 
+   *  reads rfid reader with index i
+   *  returns:
+   *    0: successful read
+   *    1: no card found
+   *    2: no signature found
+   *    3: read error
+   *    
+   *    if successful read, data will be written to *data
+   */ 
+  
+  MFRC522::StatusCode status;
+  byte buffer[18];
+  byte size = sizeof(buffer);
+  
+  reset_ss_pins();
+
+  if (!mfrc522[i].PICC_IsNewCardPresent() || !mfrc522[i].PICC_ReadCardSerial()) {
+    return 1;
+  }
+
+  // authenticate with key A, default keys (configured in setup)
+  status = (MFRC522::StatusCode) mfrc522[i].PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, DATA_TRAILER_BLOCK, &key, &(mfrc522[i].uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("PCD_Authenticate() failed: "));
+    Serial.println(mfrc522[i].GetStatusCodeName(status));
+    return 3;
+  }
+  
+  status = (MFRC522::StatusCode) mfrc522[i].MIFARE_Read(DATA_BLOCK, buffer, &size);
+  mfrc522[i].PCD_StopCrypto1();
+  
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("MIFARE_Read() failed: "));
+    Serial.println(mfrc522[i].GetStatusCodeName(status));
+    
+    return 3;
+  }
+
+  if (buffer[DATA_SIGNATURE_INDEX] != DATA_SIGNATURE) {
+    return 2;
+  }
+
+  *data = buffer[DATA_INDEX];
+  
+  return 0;
+}
+
+byte write_rfid_reader(byte i) {
+  MFRC522::StatusCode status;
+  byte buffer[18];
+  byte size = sizeof(buffer);
+
+  byte dataBlock[16] = {0};
+
+  dataBlock[DATA_SIGNATURE_INDEX] = DATA_SIGNATURE;
+  dataBlock[DATA_INDEX] = i+1;
+  
+  reset_ss_pins();
+
+  if (!mfrc522[i].PICC_IsNewCardPresent() || !mfrc522[i].PICC_ReadCardSerial()) {
+    return 1;
+  }
+
+  // authenticate with key A, default keys (configured in setup)
+  status = (MFRC522::StatusCode) mfrc522[i].PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, DATA_TRAILER_BLOCK, &key, &(mfrc522[i].uid));
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("PCD_Authenticate() failed: "));
+    Serial.println(mfrc522[i].GetStatusCodeName(status));
+    return 2;
+  }
+
+  status = (MFRC522::StatusCode) mfrc522[i].MIFARE_Write(DATA_BLOCK, dataBlock, 16);
+  mfrc522[i].PCD_StopCrypto1();
+  
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("MIFARE_Write() failed: "));
+    Serial.println(mfrc522[i].GetStatusCodeName(status));
+    return 2;
+  }
+
+  return 0;
+}
+
 void setup(void) {
   SPI.begin();
+  delay(50);
 
   for (byte i = 0; i < MFRC522_NUM; i++) {
     // reset pin is not really used by the library, putting the ss pin makes it function yet saves a pin
-    mfrc522[i] = new MFRC522(mfrc522_ss_pins[i], mfrc522_ss_pins[i]);
-    mfrc522[i]->PCD_Init();
-    mfrc522[i]->PCD_SetAntennaGain(mfrc522[i]->RxGain_avg);
+    mfrc522[i].PCD_Init(mfrc522_ss_pins[i], mfrc522_ss_pins[i]);
+    mfrc522[i].PCD_SetAntennaGain(mfrc522[i].RxGain_avg);
   }
   
   // Prepare the key (used both as key A and as key B)
@@ -42,8 +127,26 @@ void setup(void) {
   
   lcd.begin(16, 2);
 
-  Serial.begin(115200);
+  //Serial.begin(115200);
   Serial.println("Setup finished!");
+}
+
+byte is_reader_right(byte i) {
+  /*
+   * checks whether rfid reader has i+1 stored in the card
+   */
+
+  static unsigned long last_correct[MFRC522_NUM] = {};
+  byte data;
+  byte result = read_rfid_reader(i, &data);
+
+  if (result == 0) {
+    if (data == (i+1)) {
+      last_correct[i] = millis();
+    }
+  }
+
+  return (millis() - last_correct[i]) < READER_TIMEOUT;
 }
 
 char code[MAX_CODE_LENGTH];
@@ -63,23 +166,43 @@ void loop(void) {
   /* 
    * state = 0 for keypad entry
    * state = 1 for rfid card sequence
+   * state = 2 for congratulations screen
+   * state = 100 for debug
   */
   static byte state = 0;
+  static byte pState = 255;
+
+  byte state_to_set = 255;
   
-  //static unsigned long last_update = 0;
+  // counter used to track length of debug sequence entered
+  // increments for every correct character of debug sequence entered
+  static byte debug_sequence_count = 0;
   
-  //byte UID[UID_LENGTH];
   char key = keypad.getKey();
+
+  if (key == debug_sequence[debug_sequence_count]) {
+    debug_sequence_count++;
+
+    if (debug_sequence_count >= DEBUG_SEQUENCE_LENGTH) {
+      state_to_set = 100;
+    }
+  }
+  else if (key) {
+    debug_sequence_count = 0;
+  }
   
   // re-enable SPI mode. long story short, the use of pin 10 (SS) as one of the keypad pins
   // sets the SPI bus to SLAVE mode when the keypad is scanned.
   SPCR |= (1<<MSTR);
 
   if (state == 0) {
-    lcd.setCursor(0, 1);
-    lcd.print("*: Back, #:Check");
-    lcd.setCursor(0, 0);
-    lcd.print("Enter Code: ");
+    if (pState != 0) {
+      lcd.clear();
+      lcd.print("Enter Code: ");
+      lcd.setCursor(0, 1);
+      lcd.print("*: Back, #:Check");
+      code_length = 0;
+    }
     
     if (key) {
       if (key >= '0' && key <= '9') {
@@ -93,7 +216,9 @@ void loop(void) {
           code_length--;
         }
       }
-
+    
+      lcd.setCursor(12, 0);
+      
       for(byte i = 0; i < MAX_CODE_LENGTH; i++) {
         if (i < code_length) {
           lcd.print(code[i]);
@@ -108,36 +233,119 @@ void loop(void) {
         
         if (is_code_correct()) {
           lcd.print("Correct!         ");
-          state = 1;
+          state_to_set = 1;
         }
         else {
           lcd.print("Wrong Code!      ");
         }
         
         delay(1000);
+        lcd.setCursor(0, 1);
+        lcd.print("*: Back, #:Check");
       }
     }
   }
   else if (state == 1) {
-    lcd.setCursor(0, 0);
-    lcd.print("   What's the   ");
-    lcd.setCursor(0, 1);
-    lcd.print("    pattern?    ");
+    if (pState != 1) {
+      lcd.clear();
+      lcd.print("   What's the   ");
+      lcd.setCursor(0, 1);
+      lcd.print("    pattern?    ");
+    }
+    
+    byte correct = 1;
+    
+    for(byte i = 0; i < MFRC522_NUM; i++) {
+      if (!is_reader_right(i)) {
+        correct = 0;
+        break;
+      }
+    }
+
+    if (correct) state_to_set = 2;
   }
-  
-  /*for(byte i = 0; i < MFRC522_NUM; i++) {
-    reset_ss_pins();
-    if (mfrc522[i]->PICC_IsNewCardPresent() && mfrc522[i]->PICC_ReadCardSerial()) {
-      Serial.print("Found card on reader ");
-      Serial.println(i);
-      memcpy(UID, mfrc522[i]->uid.uidByte, 4);
-      print_uid(UID, 0, i);
-      mfrc522[i]->PICC_HaltA();
+  else if (state == 2) {
+    if (pState != 2) {
+      lcd.clear();
+      lcd.print("Congratulations!");
     }
-    else {
-      lcd.setCursor(0, i);
-      lcd.print("        ");
+
+    if ((keypad.findInList('*') > -1) && (keypad.findInList('#') > -1)) {
+      state_to_set = 0;
     }
-  }*/
+  }
+  else if (state == 100) {
+    static unsigned long last_state_update[MFRC522_NUM] = {0};
+    static byte last_read_state[MFRC522_NUM] = {0};
+
+    // index tracking which reader was last read
+    // used to read only one reader per cycle so buttons remain responsive
+    static byte read_index = 255;
+    
+    if (pState != 100) {
+      lcd.clear();
+      lcd.print("DEBUG");
+      lcd.setCursor(0, 1);
+      lcd.print("Readers: ");
+      Serial.println("Entering debug mode");
+    }
+
+    if (key >= '0' && key <= '2') state_to_set = key - '0';
+
+    read_index++;
+    
+    if (read_index >= MFRC522_NUM) read_index = 0;
+    
+    byte data = 0;
+    byte result = read_rfid_reader(read_index, &data);
+
+    // some "debouncing" since reads can fail while card is still there
+    // only allows not found state to propagate when last state update > READER_TIMEOUT ms ago
+    if (result == 1) {
+      if ((millis() - last_state_update[read_index]) < READER_TIMEOUT) {
+        result = last_read_state[read_index];
+      }
+    }
+    else {        
+      last_state_update[read_index] = millis();
+    }
+    
+    lcd.setCursor(9+read_index, 1);
+    switch(result) {
+      case 0:
+        if (data != 0) {
+          lcd.print(data);
+        }
+        break;
+      case 1:
+        lcd.print('?');
+        break;
+      case 2:
+        lcd.print('*');
+        break;
+      case 3:
+        lcd.print('E');
+        break;
+    }
+
+    if (result != 1) {
+      last_read_state[read_index] = result;
+    }
+
+    if (key == '*') {
+      state_to_set = 0;
+    }
+    else if (key == '#') {
+      for(byte i = 0; i < MFRC522_NUM; i++) {
+        write_rfid_reader(i);
+      }
+    }
+  }
+
+  pState = state;
+
+  if (state_to_set != 255) {
+    state = state_to_set;
+  }
 }
 
